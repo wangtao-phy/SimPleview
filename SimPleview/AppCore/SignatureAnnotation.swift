5fbf9198b1649872b896c0aea64a3c0405e172bb
wangtaoooo <taowang@mail.nankai.edu.cn>
2026-06-13 15:35:39 +0800

    Hide blurry native vector signature layer from PDFView display

--- SimPleview/AppCore/SignatureAnnotation.swift
+++ SimPleview/AppCore/SignatureAnnotation.swift
@@ -57,6 +57,12 @@ class VectorSignatureAnnotation: PDFAnnotation {
         self.themeColor = color
         super.init(bounds: bounds, forType: .stamp, withProperties: nil)
         self.color = color
+        
+        // [核心黑科技]
+        // 1. 关闭 shouldDisplay：防止 PDFKit 在屏幕上渲染出基于缓存的低清模糊版本
+        // 2. 开启 shouldPrint：确保导出/保存/打印 PDF 时，原生的纯矢量外观能够写入文件
+        self.shouldDisplay = false
+        self.shouldPrint = true
     }
     
     nonisolated override init(bounds: CGRect, forType annotationType: PDFAnnotationSubtype, withProperties properties: [AnyHashable : Any]?) {
